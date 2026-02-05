0c11a816c4327c8f9d148e670b78ad067787b505
sidd-singh04 <siddharthsingh04.singh@gmail.com>
2026-02-05 21:04:24 +0530

    profile update functionality fixed

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -1,10 +1,7 @@
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
+/**
+ * ❗ DO NOT call cloudinary.config() here
+ * Env vars are not loaded at import time
+ */
 export default cloudinary;
-
